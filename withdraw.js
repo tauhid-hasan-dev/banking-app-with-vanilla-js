e40a4f89f198f57faf62ba515bf937d9b4da3bc1
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-08-16 22:28:25 +0300

    withdraw added

--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,20 @@
 const btnWithdraw = document.getElementById('btn-withdraw');
 
 btnWithdraw.addEventListener('click', () => {
+    //get the input element and the value
+    const inputWithdraw = document.getElementById('input-withdraw');
+    const withdrawAmount = inputWithdraw.value;
 
+    //get the withdraw total element and the current inner text of it 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const currentWithdrawTotal = withdrawTotal.innerText;
+
+    // update the inner text of the withdraw total element and add to the previous total
+    withdrawTotal.innerText = +withdrawAmount + +currentWithdrawTotal;
+
+    //get the balance total
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalance = balanceTotal.innerText;
+
+    balanceTotal.innerText = +previousBalance - +withdrawAmount;
 })
